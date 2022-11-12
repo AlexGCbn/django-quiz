f52ce2ccb5f80f3bf5bb6d442d672e4b0b45c7a6
Alex Ciobanu <tsopi10@gmail.com>
2022-11-12 15:44:46 +0000

    Add base view for random questions and template

--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Category, Question
+import random
 
 
 class HomeView(View):
@@ -7,3 +9,19 @@ class HomeView(View):
     def get(self, request):
         template = 'index.html'
         return render(request, template)
+
+
+class RandomQuestionsView(View):
+
+    # GET request for random questions game
+    def get(self, request):
+        template = 'random_question.html'
+
+        question_count = Question.objects.all().count()
+        random_question = Question.objects.filter(pk=random.randint(1, question_count))
+
+        context = {
+            'question': random_question
+        }
+
+        return render(request, template, context)
